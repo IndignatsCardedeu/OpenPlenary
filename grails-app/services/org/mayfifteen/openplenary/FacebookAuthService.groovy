80fee0ceac130211abf49af54a65f33c37b37010
horrabin <nandofm@gmail.com>
2016-07-30 13:15:50 +0200

    Fixed facebook login error

--- grails-app/services/org/mayfifteen/openplenary/FacebookAuthService.groovy
+++ grails-app/services/org/mayfifteen/openplenary/FacebookAuthService.groovy
@@ -18,18 +18,17 @@
 
 package org.mayfifteen.openplenary
 
-import org.mayfifteen.openplenary.FacebookUser;
-import org.mayfifteen.openplenary.User;
-
 import com.restfb.DefaultFacebookClient
 import com.restfb.FacebookClient
+import com.restfb.Parameter
 import com.the6hours.grails.springsecurity.facebook.FacebookAuthToken
 
 class FacebookAuthService {
 
     void onCreate(FacebookUser user, FacebookAuthToken token){
 		FacebookClient fb = new DefaultFacebookClient(token.accessToken.accessToken)
-		com.restfb.types.User fbuser = fb.fetchObject("me", com.restfb.types.User.class)
+		com.restfb.types.User fbuser = fb.fetchObject("me", com.restfb.types.User.class, Parameter.with("fields","name,first_name"))
+		
 		User systemUser = User.get(user.user.id)
 		systemUser.username = fbuser.firstName + "_" + systemUser.username 
 		systemUser.save(flush:true)
